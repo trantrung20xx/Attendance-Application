2c98c6982dd95f8225a0c5bcbffd5cf4954af128
trungio <trantrung2004hy@gmail.com>
2024-12-08 12:55:56 +0700

    2 additional fingerprint matching algorithms are jaccard and cosine similarity

--- Code/Lib/uart_communication.py
+++ Code/Lib/uart_communication.py
@@ -1,5 +1,6 @@
 import serial
 import time
+from scipy.spatial.distance import cosine
 
 class UARTCommunication:
     def __init__(self, port, baudrate=115200, timeout=1):
@@ -43,6 +44,36 @@ class UARTCommunication:
         if self.serial.is_open:
             self.serial.close()
 
+def jaccard_index(data1, data2):
+    """
+        Tính chỉ số jaccard giữa 2 mẫu nhị phân
+        Tính toán bằng công thức: J(A, B) = |A & B| / |A | B |
+        Với:
+            |A & B|: Số bit giống nhau
+            |A | B|: Số bit khác nhau
+    """
+    if len(data1) == len(data2):
+        intersection = sum(bin(byte1 & byte2).count('1') for byte1, byte2 in zip(data1, data2))
+        union = sum(bin(byte1 | byte2).count('1') for byte1, byte2 in zip(data1, data2))
+        return intersection / union # Trả về tỉ lệ các bit chung giữa 2 mẫu
+    else:
+        print("Hai mẫu phải bằng nhau để so sánh")
+
+def cosine_similarity(data1, data2):
+    """
+        Tính độ tương đồng Cosine giữa 2 mẫu
+        Tính góc cosine giữa 2 vector
+        Nếu:
+            góc gần 0 độ thì hai muẫu rất giống nhau (độ tương đồng gần 1)
+            góc gần 90 độ thì hai mẫu rất khác nhau (độ tương đồng gần 0)
+    """
+    # Chuyển thành vector số
+    vector1 = list(data1)
+    vector2 = list(data2)
+    # Tính độ tương đồng (khoảng cách của 2 mẫu dữ liệu)
+    similarity = 1 - cosine(vector1, vector2) # Tính góc cosine giữa 2 vector
+    return similarity
+
 if __name__ == "__main__":
     listFingerTemplate = [b'0', b'0']
     uart = UARTCommunication(port="COM4", baudrate=115200, timeout=10)
@@ -71,8 +102,12 @@ if __name__ == "__main__":
                 print("Không nhận được phản hồi hợp lệ.")
 
             if flag % 2 == 0:
-                print(f"{listFingerTemplate[0]} - {listFingerTemplate[1]}")
-                print(listFingerTemplate[0] == listFingerTemplate[1])
+                if len(listFingerTemplate[0]) == 512 and len(listFingerTemplate[1]) == 512:
+                    print(f"{listFingerTemplate[0]} - {listFingerTemplate[1]}")
+                    jaccard_score = jaccard_index(listFingerTemplate[0], listFingerTemplate[1])
+                    similarity = cosine_similarity(listFingerTemplate[0], listFingerTemplate[1])
+                    print(f"Tỉ lệ trùng khớp với phương pháp jaccard: {jaccard_score}")
+                    print(f"Tỉ lệ trùng với với phương pháp cosine similarity: {similarity}")
 
         finally:
             # Đóng kết nối UART
